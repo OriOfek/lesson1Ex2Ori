c14a159e009db5fe4695708769b26e5152bc493a
OriOfek <aa6055@bs.amalnet.k12.il>
2020-09-04 00:39:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson1ex2ori/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lesson1ex2ori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
